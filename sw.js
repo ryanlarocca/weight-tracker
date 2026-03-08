df781c39edda0520a5463490b48255a02e758a05
Ryan LaRocca <ryanlarocca@Ryans-Mac-mini.local>
2026-03-08 10:57:42 -0700

    Fix manifest and SW paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'wt-cache-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'wt-cache-v2';
+const ASSETS = ['/weight-tracker/', '/weight-tracker/index.html', '/weight-tracker/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
