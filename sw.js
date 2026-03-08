c016572c8f98b5bda8de5f6061b3adacfa29b635
Ryan LaRocca <ryanlarocca@Ryans-Mac-mini.local>
2026-03-08 10:59:33 -0700

    Fix service worker for iOS PWA

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-const CACHE = 'wt-cache-v2';
-const ASSETS = ['/weight-tracker/', '/weight-tracker/index.html', '/weight-tracker/manifest.json'];
+const CACHE = 'wt-cache-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        '/weight-tracker/',
+        '/weight-tracker/index.html',
+        '/weight-tracker/manifest.json',
+        '/weight-tracker/icon-192.png',
+        '/weight-tracker/icon-512.png',
+      ]).catch(() => {})
+    )
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
+// Network first, fall back to cache
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
